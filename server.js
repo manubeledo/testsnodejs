9b92b4b5485b749cc0399b5130f288eaf34902d4
manubeledo <estudiocut.oficial@gmail.com>
2022-06-04 19:52:47 -0300

    with Git Ignore

--- server.js
+++ server.js
@@ -5,8 +5,4 @@ let app = express()
 
 const PORT = process.env.PORT || 3000
 
-console.log(PORT)
-
-console.log(process.env)
-
 app.listen(PORT, () => console.log(`Server working on http://localhost:${PORT}`))
